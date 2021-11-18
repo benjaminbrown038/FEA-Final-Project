7452b9269c9e7b82e9bf324881a41ccb91a0bd36
Benjamin Brown <35114142+benjaminbrown038@users.noreply.github.com>
2021-11-18 15:50:19 -0700

    documentation

--- main.py
+++ main.py
@@ -1,58 +1,66 @@
 
-G=int(input('Rectangular(1) or Cylinder(2) '))
-if G==1:
-    #completed rectangular code
-    # install sympy 
- 
-    from sympy.matrices import Matrix
- 
+# Necessary libraries for solution
+from sympy.matrices import Matrix
+from mpl_toolkits import mplot3d
+import numpy as np
+import matplotlib.pyplot as plt
 
+# Visualizing results
+def heatmap2d(arr: np.ndarray):
+    plt.imshow(arr, cmap='viridis', aspect='auto')
+    plt.colorbar()
+    plt.show()
+ 
+# Defining the fuction and equation to be used
+def f(x, y):
+    return (q*y)/(k*A)+Tf
+
+# Physical scenario is applied to a rectangle or cylinder
+G=int(input('Rectangular(1) or Cylinder(2) '))
+# rectangle equation
+if G==1:
+ 
     #insert user defined parameters
+    # material's conductivity
     k = float(input('k value '))
+    # width of rectangle
     x = float(input('width of body '))
+    # length of rectangle
     y = float(input('length of body '))
+    # depth of rectangle
     z = float(input('depth of body '))
+    # number of finite interpolation steps
     i = float(input('number of intervals per unit length '))
+    # Hot temperature
     Ti = float(input('Thot '))
+    # Cold temoerature
     Tf = float(input('Tcold '))
  
-
     # A is the surface area exposed to the heat
     A = z*x
-    #interval equations
+    # interval equations
+    # interval equation along x direction
     l=int((i*x)+1)
+    # interval equation along y direction
     w=int((i*y)+1)
- 
 
-    #Calculate the heat transfer q
+    # Heat flux (W/m^2) 
     q = k*A*(Ti -Tf)/y
-    #print('q = ' ,q)
-    #print('Ti = ' ,((q*y)/(k*A))+Tf)
 
-
-
- #input requested coordinates
-    u=1
-    while u==1:
-        xi=float(input('x coordinate: '))
-        yi=float(input('y coordinate: '))
+    # input requested coordinates
+    u = 1
+    while u == 1:
+        xi = float(input('x coordinate: '))
+        yi = float(input('y coordinate: '))
+        
         #equation for requested coordinates
-        Tr=(Ti-((q*yi/(k*A))))
+        Tr = (Ti-((q*yi/(k*A))))
         print('This is the temperature for the location you requested:',Tr)
-        o=input('Request another temperature?(Y/N)')
-        if o=="Y":
-            u=1
+        o = input('Request another temperature?(Y/N)')
+        if o == "Y":
+            u = 1
         else:
-            u=0
-
-    #importing matplotlib for graph
-    from mpl_toolkits import mplot3d
-    import numpy as np
-    import matplotlib.pyplot as plt
-
-    #defining the fuction and equation to be used
-    def f(x, y):
-        return (q*y)/(k*A)+Tf
+            u = 0
 
     #defining the x and y area of the graph, the 3rd variable is no.elements=variable-1
     x = np.linspace(0, x, l)
@@ -75,34 +83,38 @@ if G==1:
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z');
+
+# physical scenario for a hollow cyclinder    
 else:
-    #finished cylinder code
     #insert user defined parameters
+    # distance from center (closest)
     ri = float(input('Inner Radius '))
+    # distance from center (furthest)
     ro = float(input('Outer Radius '))
+    # length of cyclinder
     L = float(input('Length of Cylinder '))
+    # Temperature value closest to center
     Ti = float(input('Inner Temperature '))
+    # Temperature value furthest from center
     To = float(input('Outer Temperature '))
+    # Material's conductivity value
     k = float(input('k value '))
-    i = float(input('number of intervals per unit length '))
-
-
-    #Calculate the heat transfer q
-    import numpy as np
-    import math
-
-
+    # Number of intervals per unit length
+    i = float(input('Number of intervals per unit length '))
+    
+    # Pi and Heat transfer of hollow cyclinder
     pi = math.pi
     q = 2*pi*L*k*((Ti-To)/np.log(ro/ri))
-    print('q = ' ,q)
+    
+    # Print results
+    #print('q = ' ,q)
     #print('To = ' ,Ti-(q*np.log(ro/ri))/(2*pi*L*k))
-
 
     #input requested coordinates
     u=1
     while u==1:
         rreq=float(input('Distance from ri: '))
-        #equation for requested coordinates
+        # equation for requested coordinates
         Tr=Ti-(q*np.log(rreq/ri))/(2*pi*L*k)
         print('This is the temperature for the location you requested:',Tr)
         o=input('Request another temperature?(Y/N)')
@@ -110,28 +122,18 @@ else:
             u=1
         else:
             u=0
-        
-
-    import matplotlib.pyplot as plt
-    import numpy as np
 
 
- def heatmap2d(arr: np.ndarray):
-        plt.imshow(arr, cmap='viridis', aspect='auto')
-        plt.colorbar()
-        plt.show()
-    #creates an array of data using np.arange of size () then reshapes into a matrix using .reshape of size (x (,:by) y)
+    
+    # creates an array of data using np.arange of size () then reshapes into a matrix using .reshape of size (x (,:by) y)
     r=ri
     size=int(i*(ro-ri)+1)
-
     Tlist = []
-
     while r <= ro:
         T = Ti-(q*np.log(r/ri))/(2*pi*L*k)
         Tlist.append(T)
         r=r+(1/i)
     
-
     Tarray =  np.array(Tlist)
     Tmatrix = Tarray.reshape(1,size)
     #print(Tmatrix)
